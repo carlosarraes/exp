f1d6ab740bc8736d27c9a07692b0db66eaab1f0d
carraes <carraeshb@gmail.com>
2022-10-19 10:53:09 -0300

    build: express example

--- app.js
+++ app.js
@@ -1,38 +1,18 @@
-const http = require('http');
-const { readFileSync } = require('fs');
+const express = require('express');
+const app = express();
+const path = require ('path');
+const colors = require('colors');
 
-const homePage = readFileSync('./navbar-app/index.html', 'utf8');
-const homeStyles = readFileSync('./navbar-app/styles.css', 'utf8');
-const homeLogo = readFileSync('./navbar-app/logo.svg', 'utf8');
-const homeApp = readFileSync('./navbar-app/browser-app.js', 'utf8');
+app.use(express.static('./public'))
 
-const server = http.createServer((req,res)=>{
-  if (req.url === '/') {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(homePage);
-    res.end();
-  } else if (req.url === '/about') {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>About</h1>')
-    res.end();
-  } else if (req.url === '/styles.css') {
-    res.writeHead(200, {'Content-Type': 'text/css'});
-    res.write(homeStyles)
-    res.end();
-  } else if (req.url === '/logo.svg') {
-    res.writeHead(200, {'Content-Type': 'image/svg+xml'});
-    res.write(homeLogo)
-    res.end();
-  } else if (req.url === '/browser-app.js') {
-    res.writeHead(200, {'Content-Type': 'text/javascript'});
-    res.write(homeApp)
-    res.end();
-  }  else {
-    res.writeHead(404, {'Content-Type': 'text/html'});
-    res.write('<h1>404</h1>')
-    res.end();
-  }
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+// })
+
+app.all('*', (req, res) => {
+  res.status(404).send('Not Found');
 })
 
-console.log('Listening on port 5000...')
-server.listen(5000);
+app.listen(5000, () => {
+  console.log('Server listening on port 5000...'.blue);
+})
