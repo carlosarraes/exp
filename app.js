d80102aa30c09d88e787c914aff44112e15e4179
carraes <carraeshb@gmail.com>
2022-10-21 13:53:33 -0300

    build: learn middleware

--- app.js
+++ app.js
@@ -1,47 +1,18 @@
 const express = require('express');
 const colors = require('colors')
 const app = express();
-const { products } = require('./data')
+const logger = require('./logger');
+const authorize = require('./authorize');
 
-app.get('/', (req, res) => {
-  res.send('<h1>Home Page</h1><a href="/api/products">Products</a>')
+app.use([logger, authorize]);
+
+app.get('/', (req,res)=>{
+  res.send('Home')
 })
 
-app.get('/api/products', (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  })
-  res.json(newProducts);
-})
-
-app.get('/api/products/:productID', (req, res) => {
-  const { productID } = req.params;
-  const singleProduct = products.find((product) => product.id === Number(productID))
-  if (!singleProduct) {
-    return res.status(404).send('Product does not exist')
-  }
-  return res.json(singleProduct);
-})
-
-app.get('/api/v1/query', (req, res) => {
-  console.log("> | file: app.js | line 28 | app.get | req.params".cyan, req.query);
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => product.name.startsWith(search));
-  }
-
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-
-  if (sortedProducts.length < 1) {
-    return res.status(200).json({ success: true, data: [] })
-  }
-
-  return res.status(200).json(sortedProducts);
+app.get('/about', (req,res)=>{
+  console.log(req.user);
+  res.send('About')
 })
 
 app.listen(5000, () => {
