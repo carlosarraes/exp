b82dc2bdf8014cce157d163836b566c2ed47c9d1
carraes <carraeshb@gmail.com>
2022-10-22 11:00:45 -0300

    build: learn methods II

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ let { people } = require('./data');
 
 app.use(express.static('./public/methods-public'))
 app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
 
 app.get('/api/people', (req,res) => {
   res.status(200).json({success:true, data:people});
 });
 
 app.post('/api/people', (req,res) => {
-  res.status(201).send('success');
+  const {name} = req.body;
+  if(!name) {
+    return res.status(400).json({success:false, msg: 'Please enter a name'})
+  }
+  res.status(201).json({success:true, person: name});
 });
 
 app.post('/login', (req,res) => {
